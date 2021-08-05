310d9a6d936fc5cccff88c80f1462ddd0fb82c86
Jason Hargrove <rijaha@gmail.com>
2021-08-04 21:57:42 -0500

    Updated App.js to show bookmark title and url.

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,5 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function App(props) {
-	return <div className="AppPage">Hi from the {props.page} page</div>;
+	const [bookmarks, setBookmarks] = useState([]);
+
+	useEffect(() => {
+		(async () => {
+			try {
+				const response = await fetch('/api/bookmarks'); // <----- Postman Querry
+				const data = await response.json(); // Recieved data turn it into a js object.
+				setBookmarks(data); // Store JS object or Array.
+			} catch (error) {}
+		})();
+	}, []);
+
+	return (
+		<div className="AppPage">
+			This is the {props.page} page
+			<ul>
+				{bookmarks.map(bookmark => {
+					return (
+						<li key={bookmark._id}>
+							The bookmark is {bookmark.title} its link is {bookmark.url}
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
 }
