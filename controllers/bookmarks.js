6784451e8663baf666a65cba79252799195a7244
Jason Hargrove <rijaha@gmail.com>
2021-08-06 14:20:44 -0500

    Update completed but not tested. Starting on Delete.

--- controllers/bookmarks.js
+++ controllers/bookmarks.js
@@ -14,8 +14,35 @@ const router = require('express').Router();
           res.status(404).json({message: error.message})
       }
     })
+
   /* Show */
+    router.get(':id', async (req, res) => {
+      try {
+        const foundBookmark = await Bookmark.findById(req.params.id)
+        res.status(200).json(foundBookmark)
+      } catch (error) {
+        console.error(error)
+        res.status(404).json({message: error.message})
+      }
+    })
 
 // Update
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedBookmark = await Bookmark.findByIdAndUpdate(req.params.id, req.body, {new:true})
+    res.status(200).json(updatedBookmark)
+  } catch (error) {
+    console.error(error)
+    res.status(404).json({message: error.message})
+  }
+})
 
 // Delete
+router.delete('/:id', async (req, res) => {
+  try {
+    
+  } catch (error) {
+    console.error(error)
+    res.status(404).json({message: error.message})
+  }
+})
