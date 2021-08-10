16f56f939a4a65d26bb29e288e146ad4157d7860
Jason Hargrove <rijaha@gmail.com>
2021-08-10 13:09:09 -0500

    Redy for Heroku.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 
 const NavBar = props => {
 	return (
-		<div className="container NavBar">
+		<div className="container">
 			<nav className="navbar navbar-dark bg-dark">
-				<div className="container-fluid">
+				<div className="nav">
 					{props.routes.map(({ key, path }) => (
-						<Link key={key} to={path}>
+						<Link className="nav-link active" key={key} to={path}>
 							{key}
 						</Link>
 					))}
